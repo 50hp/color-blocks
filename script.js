add7b93e3bec36708b6382ee200bec08d6fd2fda
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-04 17:26:23 -0500

    Created 4 functions to recount the amount of colored blocks on the screen.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(onReady)
 
 function onReady() {
-    console.log('JQuery Ready');
 
 $('#red-btn').on('click', addRed);
 $('#blue-btn').on('click', addBlue);
@@ -9,41 +8,54 @@ $('#green-btn').on('click', addGreen);
 $('#yellow-btn').on('click', addYellow);
 $('#blocks').on('click','.block', removeBlock);
 
-
-
-
-
-
-
-
-
 }
+let red = 1, blue = 1, green = 1, yellow = 1;
 
 function addRed(){
-        $('#blocks').append('<div class="block red-fill"></div>')
-
+        $('#blocks').append('<div class="block red-fill"></div>');
+        $('#red-count').empty();
+        red++;
+        $('#red-count').append(red);
 }
-
 function addBlue(){
-        $('#blocks').append('<div class="block blue-fill"></div>')
-
+        $('#blocks').append('<div class="block blue-fill"></div>');
+        $('#blue-count').empty();
+        blue++;
+        $('#blue-count').append(blue);
 }
-
-
 function addGreen(){
-        $('#blocks').append('<div class="block green-fill"></div>')
-
+        $('#blocks').append('<div class="block green-fill"></div>');
+        $('#green-count').empty();
+        green++;
+        $('#green-count').append(green);
 }
-
-
 function addYellow(){
-        $('#blocks').append('<div class="block yellow-fill"></div>')
-
+        $('#blocks').append('<div class="block yellow-fill"></div>');
+        $('#yellow-count').empty();
+        yellow++;
+        $('#yellow-count').append(yellow);
 }
-
-
+function redRecount(){
+        $('#red-count').empty();
+        $('#red-count').append($('.red-fill').length);
+}
+function blueRecount(){
+        $('#blue-count').empty();
+        $('#blue-count').append($('.blue-fill').length);
+}
+function greenRecount(){
+        $('#green-count').empty();
+        $('#green-count').append($('.green-fill').length);
+}
+function yellowRecount(){
+        $('#yellow-count').empty();
+        $('#yellow-count').append($('.yellow-fill').length);
+}
 function removeBlock(){
         $(this).remove();
-
+        redRecount();
+        blueRecount();
+        greenRecount();
+        yellowRecount();
 }
 console.log('🟥 🟦 🟩 🟨');
