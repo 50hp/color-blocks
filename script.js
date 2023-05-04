ab211652d2ae9f7cb3bd4369a9f68e216d78cc8e
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-04 16:39:44 -0500

    created a function for onReady and placed 5 event liseners in the function. Created 4 add block functions and one remove block function.

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+$(onReady)
+
+function onReady() {
+    console.log('JQuery Ready');
+
+$('#red-btn').on('click', addRed);
+$('#blue-btn').on('click', addBlue);
+$('#green-btn').on('click', addGreen);
+$('#yellow-btn').on('click', addYellow);
+$('#blocks').on('click','.block', removeBlock);
+
+
+
+
+
+
+
+
+
+}
+
+function addRed(){
+        $('#blocks').append('<div class="block red-fill"></div>')
+
+}
+
+function addBlue(){
+        $('#blocks').append('<div class="block blue-fill"></div>')
+
+}
+
+
+function addGreen(){
+        $('#blocks').append('<div class="block green-fill"></div>')
+
+}
+
+
+function addYellow(){
+        $('#blocks').append('<div class="block yellow-fill"></div>')
+
+}
+
+
+function removeBlock(){
+        $(this).remove();
+
+}
 console.log('🟥 🟦 🟩 🟨');
